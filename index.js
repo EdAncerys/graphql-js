1562ca4fc4e720f59e0990e88a0e21729b8b1052
Ed <lookatemail@gmail.com>
2022-06-14 16:56:50 +0100

    Add type def & resolvers & able to query data via playground

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// --------------------------------------------------------------------------------
+// 📌  Init Apollo Server
+// --------------------------------------------------------------------------------
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});

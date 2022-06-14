17f8cef9248cf967a93dcc138c587995b30fba23
Ed <lookatemail@gmail.com>
2022-06-14 17:59:13 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,13 @@
+## 🚀 App to test & use Graph QL queries in sandbox applications
+
+#### To run the app
+
+```
+yarn dev
+```
+
+#### To install dependencies (see package.json)
+
+```
+yarn
+```
--- index.js
+++ index.js
@@ -8,12 +8,24 @@ const { ApolloServer, gql } = require('apollo-server');
 // Scaler types: String, Int, Float, Boolean
 
 const typeDefs = gql`
+  # Define your scalar types here
   type Query {
     hello: String! # ! means that this is a required field
     stringArray: [String!]!
     numberOfRows: Int
     float: Float
     isFalse: Boolean
+
+    product: [Product!]!
+  }
+
+  # Define your schema here
+  type Product {
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    inStock: Boolean!
   }
 `;
 
@@ -24,6 +36,22 @@ const resolvers = {
     float: () => 1.23456789,
     isFalse: () => false,
     stringArray: () => ['a', 'b', 'c'],
+    product: () => [
+      {
+        name: 'Product 1',
+        description: 'Description 1',
+        quantity: 1,
+        price: 1.99,
+        inStock: true,
+      },
+      {
+        name: 'Product 2',
+        description: 'Description 2',
+        quantity: 2,
+        price: 2.99,
+        inStock: false,
+      },
+    ],
   },
 };
 

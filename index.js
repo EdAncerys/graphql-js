84c829de8bb1ec140d546366c040d266814301a3
Ed <lookatemail@gmail.com>
2022-06-14 17:42:04 +0100

    Add diff data types to typeDef

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const { ApolloServer, gql } = require('apollo-server');
 
 // --------------------------------------------------------------------------------
 // 📌  Init Apollo Server
+// Scaler types and resolvers are defined in the schema.js file
 // --------------------------------------------------------------------------------
+
+// Scaler types: String, Int, Float, Boolean
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello: String! # ! means that this is a required field
+    stringArray: [String!]!
+    numberOfRows: Int
+    float: Float
+    isFalse: Boolean
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
+    numberOfRows: () => 100,
+    float: () => 1.23456789,
+    isFalse: () => false,
+    stringArray: () => ['a', 'b', 'c'],
   },
 };
 

53400cbdbbff4c3b3a92b40e3548268662376403
Ed <lookatemail@gmail.com>
2022-06-26 17:18:40 +0100

    Add query lookup for singular item

--- index.js
+++ index.js
@@ -1,4 +1,88 @@
 const { ApolloServer, gql } = require('apollo-server');
+// --------------------------------------------------------------------------------
+const products = [
+  {
+    id: '53a0724c-a416-4cac-ae45-bfaedce1f147',
+    name: 'Steel Pot',
+    description: 'Silver steel pot that is perfect for cooking',
+    quantity: 230,
+    price: 42.44,
+    image: 'img-1',
+    onSale: false,
+  },
+  {
+    id: 'c2af9adc-d0b8-4d44-871f-cef66f86f7f6',
+    name: 'Salad Bowl',
+    description: 'Round wooden bowl perfect for tossing and making salads',
+    quantity: 33,
+    price: 53.5,
+    image: 'img-2',
+    onSale: false,
+  },
+  {
+    id: '2c931e7e-510f-49e5-aed6-d6b44087e5a1',
+    name: 'Spoon',
+    description: 'Small and delicate spoon',
+    quantity: 4266,
+    price: 1.33,
+    image: 'img-3',
+    onSale: true,
+  },
+  {
+    id: '404daf2a-9b97-4b99-b9af-614d07f818d7',
+    name: 'Shovel',
+    description: 'Grey rounded shovel for digging',
+    quantity: 753,
+    price: 332,
+    image: 'img-4',
+    onSale: false,
+  },
+  {
+    id: '6379c436-9fad-4b3f-a427-2d7241f5c1b1',
+    name: 'Fertilizer',
+    description: 'Nitrogen based fertitlizer',
+    quantity: 53453,
+    price: 23.11,
+    image: 'img-5',
+    onSale: true,
+  },
+  {
+    id: 'f01bcdec-6783-464e-8f9e-8416830f7569',
+    name: 'Basketball',
+    description: 'Outdoor or indoor basketball',
+    quantity: 128,
+    price: 59.99,
+    image: 'img-6',
+    onSale: true,
+  },
+  {
+    id: 'a4824a31-5c83-42af-8c1b-6e2461aae1ef',
+    name: 'Golf Clubs',
+    description: 'Good for golfing',
+    quantity: 3,
+    price: 427.44,
+    image: 'img-7',
+    onSale: false,
+  },
+  {
+    id: 'b553085a-a7e0-4c9b-8a12-f971919c3683',
+    name: 'Baseball Gloves',
+    description: 'Professional catcher gloves',
+    quantity: 745,
+    price: 77.0,
+    image: 'img-8',
+    onSale: true,
+  },
+  {
+    id: '47bf3941-9c8b-42c0-9c72-7f3985492a5b',
+    name: 'Soccer Ball',
+    description: 'Round ball',
+    quantity: 734,
+    price: 93.44,
+    image: 'img-9',
+    onSale: false,
+  },
+];
 
 // --------------------------------------------------------------------------------
 // 📌  Init Apollo Server
@@ -16,18 +100,28 @@ const typeDefs = gql`
     float: Float
     isFalse: Boolean
 
-    product: [Product!]!
+    products: [Product!]!
+    product(id: ID!): Product
   }
 
   # Define your schema here
   type Product {
+    id: ID
     name: String!
     description: String!
     quantity: Int!
     price: Float!
     inStock: Boolean!
+    image: String!
   }
 `;
+
+const getSingleProduct = (parent, args, context) => {
+  const productId = args.id;
+  console.log('🐞 ', productId); // debug
+
+  return data.find((product) => product.id === productId);
+};
 
 const resolvers = {
   Query: {
@@ -36,22 +130,16 @@ const resolvers = {
     float: () => 1.23456789,
     isFalse: () => false,
     stringArray: () => ['a', 'b', 'c'],
-    product: () => [
-      {
-        name: 'Product 1',
-        description: 'Description 1',
-        quantity: 1,
-        price: 1.99,
-        inStock: true,
-      },
-      {
-        name: 'Product 2',
-        description: 'Description 2',
-        quantity: 2,
-        price: 2.99,
-        inStock: false,
-      },
-    ],
+    products: () => products,
+    product: (parent, args, context) => {
+      const productId = args.id;
+      const product = products.find((product) => product.id === productId);
+      console.log('🐞 ', products);
+      console.log('🐞 ', product);
+      if (!product) return null;
+      return product;
+    },
+    // product: (parent, args, context) => getSingleProduct(parent, args, context),
   },
 };
 
